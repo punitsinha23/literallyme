71b73c77042ca1f78ff3009a8a6e391171ced91f
punit sinha <162338818+punitsinha23@users.noreply.github.com>
2024-12-14 17:05:36 +0000

    added static files

--- literallyme/settings.py
+++ literallyme/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,10 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# URL for serving static files
+STATIC_URL = '/static/'
+
+# Directories containing static files to be collected
 STATICFILES_DIRS = [
-    BASE_DIR / "static" , 
+    BASE_DIR / "static",  # Your custom static files
 ]
+
+# Directory where collected static files will be stored
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Storage backend for managing static files with Whitenoise
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 # Default primary key field type
